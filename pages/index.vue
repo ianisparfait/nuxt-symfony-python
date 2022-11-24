e46a1f239b9a77d38f2ed296971a9ff8e9b1fc21
Ianis PARFAIT <ianis.google@formationfim.onmicrosoft.com>
2022-11-24 16:06:16 +0100

    starting home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="container_page">
+  <div class="container_page proj_home">
     <div class="header">
       <Header />
     </div>
+    <section class="proj_home_hero">
+      <img :src="require('./../assets/resources/images/home/hero.jpg')" alt="3 voitures alignée de la marque Maserrati">
+      <div class="proj_home_hero_text">
+        <p>
+          <span class="arrow">▷</span> Depuis 2008, RIDE, agence de location de voitures de luxe propose ses services partout en France
+          (Paris, Monaco, Nice, Cannes, Saint-Tropez, Courchevel, Saint-Moritz...).
+          Notre expérience est à votre service pour répondre à toutes vos demandes
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
