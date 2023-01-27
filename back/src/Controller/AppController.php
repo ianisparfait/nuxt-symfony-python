7e8d0dac8ee7ad3fc3fb1c7e9988de79517063cc
Ianis PARFAIT <ianis.google@formationfim.onmicrosoft.com>
2023-01-27 18:15:44 +0100

    futur user endpoints

--- back/src/Controller/AppController.php
+++ back/src/Controller/AppController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\FutureUser;
+use App\Repository\FutureUserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AppController extends AbstractController
 {
     private EntityManagerInterface $em;
+    private FutureUserRepository $futurUser_repo;
 
-    public function __construct(EntityManagerInterface $em) {
+    public function __construct(EntityManagerInterface $em, FutureUserRepository $futur_u_r) {
         $this->em = $em;
+        $this->futurUser_repo = $futur_u_r;
     }
 
     /**
@@ -62,7 +65,38 @@ class AppController extends AbstractController
      */
     public function registerUserAdmin(string $id): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
-        $u = $data["user"];
+        $user = $this->getUser();
+
+        if ($user->getRoles()[0] !== 'ROLE_ADMIN') {
+            $response = new JsonResponse("Not an admin user", 400);
+            return $response->send();
+        }
+
+        $futurU = $this->futurUser_repo->findOneBy(["id", $id]);
+
+        $this->passFutureUserToUser();
+
+        return $this->json([
+            "status" => 200,
+            "message" => "Futur user accepted with success!"
+        ]);
+    }
+
+    private function passFutureUserToUser(string $email, FutureUser $futurUser) {
+        $user = new User();
+
+        $user->setEmail($email);
+        $user->setRoles(["ROLE_USER"]);
+
+        $plainTextPassword = $this->getParameter('defaultPassword');
+        $hasher = $this->password_hasher->hashPassword($user, $plainTextPassword);
+        $user->setPassword($hasher);
+
+        $futurUser->setIsValid(true);
+
+        $this->em->persist($futurUser);
+        $this->em->persist($user);
+
+        $this->flush();
     }
 }
